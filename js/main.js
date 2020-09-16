b6efc7736f628e86b0b8d7d1c44f66993bf23798
Neksov <n2v@list.ru>
2020-09-16 23:54:04 +0300

    new block

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 //бургер меню
-const menuToggle = document.querySelector("#menu-togle");
+const menuToggle = document.querySelector(".menu-icon");
 const mobileNavContainer = document.querySelector(".menu__mobile");
 
 menuToggle.onclick = function () {
   menuToggle.classList.toggle("menu-icon-active");
   mobileNavContainer.classList.toggle("menu__mobile--active");
 };
+
 $(".nav__item").click(function (event) {
   $(".menu-icon").removeClass(".menu-icon-active");
   $(".menu__mobile").removeClass(".menu__mobile--active");
