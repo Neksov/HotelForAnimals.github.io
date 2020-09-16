d76caccb966bcaeddea7a7fe98b8fdbcf1947add
Neksov <n2v@list.ru>
2020-09-16 13:34:03 +0300

    add mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //бургер меню
 const menuToggle = document.querySelector("#menu-togle");
-const mobileNavContainer = document.querySelector(".menu__nav");
+const mobileNavContainer = document.querySelector(".menu__mobile");
 
 menuToggle.onclick = function () {
   menuToggle.classList.toggle("menu-icon-active");
-  mobileNavContainer.classList.toggle("menu__nav--active");
+  mobileNavContainer.classList.toggle("menu__mobile--active");
 };
 $(".nav__item").click(function (event) {
   $(".menu-icon").removeClass(".menu-icon-active");
-  $(".menu__nav").removeClass(".menu__nav--active");
+  $(".menu__mobile").removeClass(".menu__mobile--active");
 });
